82efbbf6135ccf74cdfe43de3c99b029f46668fc
Joonwoo Lee <jlee228@ncsu.edu>
2023-10-07 14:32:57 -0400

    set width, not minWidth

--- src/components/table/columnsConfig.ts
+++ src/components/table/columnsConfig.ts
@@ -1,10 +1,10 @@
 import { GridColDef } from "@mui/x-data-grid/models/colDef/gridColDef";
 
 const columns: GridColDef[] = [
-  { field: "name", headerName: "Donor's Name", minWidth: 170 },
-  { field: "type", headerName: "Donation Type", minWidth: 100 },
-  { field: "quantity", headerName: "Quantity", type: "number", minWidth: 100 },
-  { field: "date", headerName: "Date", minWidth: 100 },
+  { field: "name", headerName: "Donor's Name", width: 170 },
+  { field: "type", headerName: "Donation Type", width: 200 },
+  { field: "quantity", headerName: "Quantity", width: 150 },
+  { field: "date", headerName: "Date", width: 100 },
 ];
 
 export default columns;
