adc93b03367104e44d5214100ba783237204c35a
Joonwoo Lee <jlee228@ncsu.edu>
2023-10-08 20:51:00 -0400

    enable color theme based on system preference

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { Tab, Tabs } from "@mui/material";
+import { Tab, Tabs, useMediaQuery } from "@mui/material";
 import Box from "@mui/material/Box/Box";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import createTheme from "@mui/material/styles/createTheme";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import "./App.css";
 import Distribution from "./components/distribution/Distribution";
 import Registration from "./components/registration/Registration";
@@ -11,19 +11,24 @@ import Reports from "./components/reports/Reports";
 function App() {
   const [value, setValue] = useState<number>(0);
 
-  // TODO: add toggle for dark mode
-  const darkTheme = createTheme({
-    palette: {
-      mode: "dark",
-    },
-  });
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
 
   const handleChange = (_: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
 
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       <Box sx={{ width: "100%", height: "100%" }}>
         <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
           <Tabs value={value} onChange={handleChange}>
