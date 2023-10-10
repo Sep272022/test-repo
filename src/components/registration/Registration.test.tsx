57983f3ad49bd9ced6b617dccbcd820159008e28
Joonwoo Lee <jlee228@ncsu.edu>
2023-10-10 11:21:33 -0400

    WIP: set up testing

---
+++ src/components/registration/Registration.test.tsx
@@ -0,0 +1,58 @@
+import "@testing-library/jest-dom/extend-expect";
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react";
+import { saveDonation } from "../../utils/apiClient";
+import Registration from "./Registration";
+
+// Mocking the saveDonation function since we don't actually want to call the API during tests
+jest.mock("../../utils/apiClient", () => ({
+  saveDonation: jest.fn(),
+}));
+
+describe("Registration", () => {
+  it("renders correctly", () => {
+    const { getByText } = render(<Registration />);
+    expect(getByText("Submit")).toBeInTheDocument();
+  });
+
+  it("displays an error message when trying to submit an empty form", async () => {
+    const { getByText, findByText } = render(<Registration />);
+
+    fireEvent.click(getByText("Submit"));
+
+    const errorMessage = await findByText("Name is required");
+    expect(errorMessage).toBeInTheDocument();
+  });
+
+  it("submits the form correctly with valid data", async () => {
+    (saveDonation as jest.Mock).mockResolvedValueOnce(undefined);
+
+    render(<Registration />);
+
+    fireEvent.change(screen.getByLabelText("Name"), {
+      target: { value: "John Doe" },
+    });
+    fireEvent.mouseDown(screen.getByLabelText("Type"));
+    const listItem = await screen.findByText("Food");
+    fireEvent.click(listItem);
+    fireEvent.change(screen.getByLabelText("Quantity"), {
+      target: { value: "5" },
+    });
+    // Further interactions can be added for the DatePicker if necessary, but for brevity, I'm focusing on other inputs
+
+    await act(async () => {
+      fireEvent.click(screen.getByText("Submit"));
+    });
+
+    await waitFor(() => {
+      expect(saveDonation).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  // ... More tests can be added for other interactions and scenarios ...
+});
