c9bb5efe3405a7f7c8f98244ffb75037c4bf36d1
Joonwoo Lee <jlee228@ncsu.edu>
2023-10-26 23:15:56 -0400

    Add a check for invalid donation

--- backend/services/donationService.js
+++ backend/services/donationService.js
@@ -1,8 +1,12 @@
 const DonorService = require("./donorService");
 const DonationTypeService = require("./donationTypeService");
+const { donationRequestValidator } = require("../utils/validate");
 
 const TABLE_NAME = "Donations";
 const makeDonation = async (db, { name, type, quantity, date }) => {
+  if (!donationRequestValidator({ name, type, quantity, date })) {
+    throw new Error("Invalid donation");
+  }
   const TypeID = await DonationTypeService.getId(db, type);
 
   const donatorId = await DonorService.getDonatorId(db, name);
