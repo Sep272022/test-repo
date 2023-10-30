0167665092f5a39bfbf1ad654bc8386df8a7ae8f
Joonwoo Lee <jlee228@ncsu.edu>
2023-10-30 18:08:45 -0400

    refactored

--- src/components/registration/Registration.test.tsx
+++ src/components/registration/Registration.test.tsx
@@ -10,7 +10,9 @@ jest.mock("../../utils/apiClient", () => ({
 
 const fillField = async (label: string, value: string) => {
   const input = screen.getByLabelText(label);
-  await userEvent.type(input, value);
+  await act(async () => {
+    await userEvent.type(input, value);
+  });
 };
 
 const selectItem = async (label: string, value: string) => {
@@ -19,11 +21,8 @@ const selectItem = async (label: string, value: string) => {
   await userEvent.click(listItem);
 };
 
-const clickSubmitAndCheckError = async (
-  submitText: string,
-  errorText: string
-) => {
-  await userEvent.click(screen.getByText(submitText));
+const clickSubmitAndCheckError = async (errorText: string) => {
+  await userEvent.click(screen.getByText("Submit"));
   const errorMessage = await screen.findByText(errorText);
   expect(errorMessage).toBeInTheDocument();
 };
@@ -37,19 +36,19 @@ describe("Registration", () => {
   it("displays an error message when trying to submit an empty form", async () => {
     render(<Registration />);
 
-    await clickSubmitAndCheckError("Submit", "Name is required");
+    await clickSubmitAndCheckError("Name is required");
 
     await fillField("Name", "John Doe");
-    await clickSubmitAndCheckError("Submit", "Type is required");
+    await clickSubmitAndCheckError("Type is required");
 
     await selectItem("Type", "Food");
-    await clickSubmitAndCheckError("Submit", "Quantity is required");
+    await clickSubmitAndCheckError("Quantity is required");
 
     await fillField("Quantity", "5");
-    await clickSubmitAndCheckError("Submit", "Date is required");
+    await clickSubmitAndCheckError("Date is required");
   });
 
-  it("submits the form correctly with valid data", async () => {
+  it("submits the form correctly with valid data", (done) => {
     const mockedSaveDonation = saveDonation as jest.MockedFunction<
       typeof saveDonation
     >;
@@ -58,21 +57,23 @@ describe("Registration", () => {
 
     render(<Registration />);
 
-    await fillField("Name", "John Doe");
-    await selectItem("Type", "Food");
-    await fillField("Quantity", "5");
-    await fillField("Date", "04012021");
+    fillField("Name", "John Doe");
+    selectItem("Type", "Food");
+    fillField("Quantity", "5");
+    fillField("Date", "04012021");
 
-    await act(async () => {
-      userEvent.click(screen.getByText("Submit")).then(() => {
-        expect(saveDonation).toHaveBeenCalledTimes(1);
-        expect(mockedSaveDonation).toHaveBeenCalledWith({
-          name: "John Doe",
-          type: "Food",
-          quantity: 5,
-          date: "2021-04-01",
-        });
+    act(async () => {
+      await userEvent.click(screen.getByText("Submit"));
+    }).then(() => {
+      expect(saveDonation).toHaveBeenCalledTimes(1);
+      expect(mockedSaveDonation).toHaveBeenCalledWith({
+        name: "John Doe",
+        type: "Food",
+        quantity: 5,
+        date: "2021-04-01",
       });
     });
+
+    done();
   });
 });
